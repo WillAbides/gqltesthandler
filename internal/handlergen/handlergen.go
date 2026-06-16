538893ec9887860bbea1b98de5cf7772cfc318f5
Alessandro Baccin <alebaccin@github.com>
2026-06-16 16:55:08 +0100

    use switch in handlergen.go:194

--- internal/handlergen/handlergen.go
+++ internal/handlergen/handlergen.go
@@ -191,16 +191,17 @@ func extractSelectionFieldsInto(schema *ast.Schema, selSet ast.SelectionSet, pre
 				JSONName:    sel.Name,
 			}
 
-			if isObject && len(sel.SelectionSet) > 0 {
+			switch {
+			case isObject && len(sel.SelectionSet) > 0:
 				nestedTypeName := prefix + exportedName(sel.Name)
 				sf.TypeName = nestedTypeName
 				sf.NestedFields = extractSelectionFields(schema, sel.SelectionSet, nestedTypeName)
 				sf.GoType = wrapGoType(sel.Definition.Type, nestedTypeName)
-			} else if sel.Name == "__typename" {
+			case sel.Name == "__typename":
 				// The `__typename` meta-field is `String!` (non-null) per the
 				// GraphQL spec, so emit a plain string rather than *string.
 				sf.GoType = "string"
-			} else {
+			default:
 				sf.GoType = goTypeForGraphQL(schema, sel.Definition.Type)
 			}
 
